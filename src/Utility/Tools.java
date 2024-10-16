679a3985f15911c0cabe07876aa53afb3f29041f
ayce777 <aycee2001coban@gmail.com>
2024-10-16 11:20:01 +0300

    Utility kısmı düzeltildi

--- src/Utility/Tools.java
+++ src/Utility/Tools.java
@@ -1,4 +1,19 @@
 package Utility;
 
+import javax.lang.model.util.Elements;
+
 public class Tools {
+
+    public static void Bekle(int sn){
+
+        try {
+            Thread.sleep(sn*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void Login(){
+        Elements e =new Elements();
+    }
 }
